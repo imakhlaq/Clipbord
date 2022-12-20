9f94c7ed1fff192083f3e7f739396b3e07240a13
imakhlaq <imakhlaqXD@gmail.com>
2022-12-20 17:19:35 +0530

    super-charge section

--- src/components/UI/SuperCharge.js
+++ src/components/UI/SuperCharge.js
@@ -1,4 +1,6 @@
 import blacklist from "../images/icon-blacklist.svg";
+import preview from "../images/icon-preview.svg";
+import text1 from "../images/icon-text.svg";
 
 const SuperCharge = () => {
   return (
@@ -10,8 +12,29 @@ const SuperCharge = () => {
         </p>
       </div>
       <div className="flex flex-col items-center justify-center md:flex-row space-y-16 md:space-y-0 md:space-x-12">
-        <div className="flex flex-col md:flex-row items-center justify-center space-y-5">
+        <div className="flex flex-col items-center justify-center space-y-5">
           <img src={blacklist} alt="" className="mb-6" />
+          <h5>Create Backlist</h5>
+          <p className="max-w-lg text-xl text-grayishBlue text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+            nesciunt.
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-5">
+          <img src={preview} alt="" className="mb-6" />
+          <h5>Sneak Preview</h5>
+          <p className="max-w-lg text-xl text-grayishBlue text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+            nesciunt.
+          </p>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-5">
+          <img src={text1} alt="" className="mb-6" />
+          <h5>Plain Simple Snippet</h5>
+          <p className="max-w-lg text-xl text-grayishBlue text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+            nesciunt.
+          </p>
         </div>
       </div>
     </section>
